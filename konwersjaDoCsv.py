6d9ade5b379cebb6f8c675a957087ccdbed043d2
barki <01199235@pw.edu.pl>
2025-12-13 01:11:12 +0100

    dodanie swoich partii i dodanie do konwersji output2

--- konwersjaDoCsv.py
+++ konwersjaDoCsv.py
@@ -16,3 +16,22 @@ result.print_summary()
 
 
 # %%
+dirname2 = "chess_games\\bartek"
+games2 = os.listdir(dirname2)
+
+for i in range(0, len(games2)):
+    games2[i] = dirname2 + "\\" + games2[i]
+
+# %%
+pgn_data = PGNData(games2, "output2")
+result = pgn_data.export()
+result.print_summary()
+
+
+# Zapisuję w output2 a nie w output, ponieważ github nie zbiera plików powyżej 100
+# 100 MB. Proponuję, żeby później wrzucić te ramki do SQL pewnie, że by szybciej się otwierały.
+# można też zrobić tak prowizorycznie na razie, że każdy będzie trzymał w folderze nad tym i każdy oddzielnie zmerguje sobie te pgn.
+# W skrócie możemy to jeszcze ustalić. Niektóre kolumny w sumie i tak będzie można wyrzucić,
+# np. uct time. Ale i tak output_moves2 jest zbyt duży.
+#I jestem za sqlite3 do przechowywania ramek danych. Powinno być łatwiej.
+## %
