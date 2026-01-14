6208d26e1922e74c520019fa9b32d80fcbbca94a
barki <01199235@pw.edu.pl>
2026-01-14 20:03:47 +0100

    cos

--- WykresyB.R
+++ WykresyB.R
@@ -6,6 +6,20 @@ library(RColorBrewer)
 df <- read.csv("output2_game_info.csv")
 nazwy_bartka <- c("BarMan-ek", "bArmAnEk")
 
+kolory <- c(
+  "FirejFox" = "orange",
+  "GDgamers" = "blue",
+  "BarMan-ek" = "violet",
+  "bArmAnEk" = "violet"
+)
+
+########## Załadowanie danych
+
+df_ruchy <- read.csv("TWD_Projekt2\\full_moves.csv")
+df_dane_partii <- read.csv("TWD_Projekt2\\full_game_info.csv")
+View(df_dane_partii)
+View(df_ruchy)
+
 ###################### Bardzo źle wyglądające wykresy kołowe
 ######## Wykresy kołowe dzielące partie na ilość porażek, zwycięstw oraz remisów
 
@@ -32,3 +46,10 @@ pie(df2$n, labels = df2$wynik, border = "white", col = c("orange", "red", "green
 
 
 ####################
+
+
+
+l1 <- df_ruchy %>% filter(move_no == 1) %>% select()
+
+
+
